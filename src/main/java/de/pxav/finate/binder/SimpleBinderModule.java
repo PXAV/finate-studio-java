96bc5fc994f670b167b8bf1f0383b1f851e62d67
pxav <pxav.studios@gmail.com>
2020-02-08 19:59:42 +0100

    Update documentation for SimpleBinderModule

--- src/main/java/de/pxav/finate/binder/SimpleBinderModule.java
+++ src/main/java/de/pxav/finate/binder/SimpleBinderModule.java
@@ -15,14 +15,13 @@ import java.util.concurrent.Executors;
  * which binds essential instances like the main frame.
  *
  * The main frame is injected into every window class and modified
- * by the
- *
- *
+ * by the individual window classes (implementing {@code WindowTemplate}).
  *
  * @author pxav
  */
 public final class SimpleBinderModule extends AbstractModule {
 
+  // the main frame which is used throughout the entire project.
   private final JFrame mainFrame;
 
   public SimpleBinderModule(JFrame mainFrame) {
@@ -31,6 +30,8 @@ public final class SimpleBinderModule extends AbstractModule {
 
   /**
    * Configures the abstract Guice injector module.
+   *
+   * This allows you to
    */
   @Override
   protected void configure() {
